ce79128661082a32d62679b70c3ee27ca0648025
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-03-20 15:47:48 +0000

    Cakes can be sold and counted

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,18 @@
 import "./Recipe.css"
 
-const Recipe = ({cakeName, ingredients, price, rating}) => {
+const Recipe = ({cakeName, ingredients, price, rating, count, onButtonClick}) => {
+
+    const handleClick = () => {
+        onButtonClick(count + 1);
+    }
 
     return (
         <div className="recipe">
             <h3>{cakeName}</h3>
             <ul id="ingedients-list">Ingredients: {ingredients.join(", ")}</ul>
-            <h5>Price: £{price}</h5>
-            <h5>Rating: {rating}</h5>
+            <h5>Price: £{price}, Rating: {rating}</h5>
+            <h5>Cakes sold: {count}</h5>
+            <button onClick={handleClick}>Sell cake</button>
         </div>
     );
 }
--- src/containers/RecipeContainer.js
+++ src/containers/RecipeContainer.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import Recipe from "../components/Recipe";
 
 const RecipeContainer = () => {
+
+    const [victoriaCount, setVictoriaCount] = useState(0);
+    const [teaLoafCount, setTeaLoafCount] = useState(0);
+    const [carrotCount, setCarrotCount] = useState(0);
+
     return (
         <>
             <h2>Recipes:</h2>
@@ -9,18 +15,25 @@ const RecipeContainer = () => {
                         ingredients={["eggs", "butter", "sugar", "self-raising flour",
                                     "baking powder", "milk"]}
                         price={5}
-                        rating={5} />
+                        rating={5}
+                        count={victoriaCount}
+                        onButtonClick={setVictoriaCount}/>
                 <Recipe cakeName={"Tea Loaf"} 
                         ingredients={["eggs", "oil", "dried fruit", "sugar", "self-raising flour",
                                     "strong tea"]}
                         price={2}
-                        rating={3} />
+                        rating={3}
+                        count={teaLoafCount}
+                        onButtonClick={setTeaLoafCount}/>
                 <Recipe cakeName={"Carrot Cake"} 
                         ingredients={["carrots", "walnuts", "oil", "cream cheese",
                                     "flour", "sugar"]}
                         price={8}
-                        rating={5} />
+                        rating={5}
+                        count={carrotCount}
+                        onButtonClick={setCarrotCount}/>
             </section>
+            <h2>Total cakes sold: {victoriaCount + teaLoafCount + carrotCount}</h2>
         </>
     );
 }
